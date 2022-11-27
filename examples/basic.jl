18928d43c3bdcc5753f4deaffa98a61d9a1a84ba
Jorge Pereira <jorge.brito.xyz@gmail.com>
2022-11-27 20:22:45 -0300

    docs: add comments to explain some things

--- examples/basic.jl
+++ examples/basic.jl
@@ -1,12 +1,18 @@
 using Tela
 
-setting"title"  = "Basic Example"
+# Use the setting" macro to set common application settings,
+# such as the window title, width, height, etc...
+
+setting"title"  = "[Example] Basic"
 setting"width"  = 800
 setting"height" = 600
 
+# setup will be called once the application is initialized.
 setup(::App) = background(rgb(12))
 
+# update will be called every frame.
 function update(::App)
+    # Check if a particular mouse button is being pressed
     if isdown(mouse"left")
         hue = 360 * (cos(π/12 * @seconds) + 1)
         fillcolor(hsl(hue, 0.75, 0.65))
@@ -25,4 +31,12 @@ function update(::App)
     text("Hold Mouse Left 🖱️ to draw | Press Mouse Right 🖱️ to clear", 10, 10)
 end
 
+# Runs the application.
+#
+# Basically, this macro will look at the functions you defined and
+# add then as callback listeners to specific Tela events, based on
+# their name.
+#
+# So for example, the setup and update functions defined above will
+# be added to the setup and update Tela events, respectively.
 Tela.@run()
